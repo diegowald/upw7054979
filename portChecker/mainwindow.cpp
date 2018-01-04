549fd942748c312bd148d2f3fa79cb20742df87c
Diego Ignacio Wald <diego.wald@gmail.com>
2018-01-04 17:58:28 -0300

    Implementing logic for response.

--- portChecker/mainwindow.cpp
+++ portChecker/mainwindow.cpp
@@ -1,12 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->lblResult->clear();
 }
 
 MainWindow::~MainWindow()
@@ -16,21 +16,22 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-        QString url = "https://portchecker.co/check";
-        HttpRequestInput req(url, "POST");
+    QString url = "https://portchecker.co/check";
+    HttpRequestInput req(url, "POST");
 
-        req.add_var("target_ip", ui->txtURL->text());
-        req.add_var("port", ui->txtPort->text());
+    req.add_var("target_ip", ui->txtURL->text());
+    req.add_var("port", ui->txtPort->text());
 
-        HttpRequestWorker *worker = new HttpRequestWorker(this);
-        connect(worker, &HttpRequestWorker::on_execution_finished, this, &MainWindow::on_execution_finished);
+    HttpRequestWorker *worker = new HttpRequestWorker(this);
+    connect(worker, &HttpRequestWorker::on_execution_finished, this, &MainWindow::on_execution_finished);
 
-        worker->execute(&req);
+    worker->execute(&req);
 }
 
 void MainWindow::on_execution_finished(HttpRequestWorker *worker)
 {
     QString msg;
+    bool err = false;
     if (worker->error_type == QNetworkReply::NoError)
     {
         // communication was successful
@@ -40,10 +41,26 @@ void MainWindow::on_execution_finished(HttpRequestWorker *worker)
     {
         // an error occurred
         msg = worker->error_str;
+        err = true;
     }
     qDebug() << worker->error_type;
     qDebug() << msg;
-    ui->lblResult->setText(msg);
-    worker->deleteLater();
 
+    if (err)
+    {
+        ui->lblResult->setText("Error: " + msg);
+    }
+    else
+    {
+        if (msg.contains("Port 8080 is <span class=\"red\">closed</span>"))
+        {
+            ui->lblResult->setText("Closed");
+        }
+        else
+        {
+            ui->lblResult->setText("Closed");
+        }
+    }
+
+    worker->deleteLater();
 }
